dc8ce497692ab3743e72caa4a5650ea6309f81f8
noeavl <lopeznoe191119@gmail.com>
2025-10-18 11:53:03 -0600

    Swagger documentación de la RESTFull API y cambio en la ruta de node dist/main - > node dist/src/main

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsMongoId,
   IsNumber,
@@ -10,13 +11,17 @@ import {
 export class CreateBookDto {
   @IsString()
   @IsMongoId()
+  @ApiProperty()
   authorId: string;
+  @ApiProperty()
   @IsString()
   title: string;
+  @ApiProperty()
   @IsNumber()
   @Min(1)
   @Max(new Date().getFullYear() + 5)
   publicationYear: number;
+  @ApiProperty()
   @IsString()
   @IsMongoId()
   @IsOptional()
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Biblioteca Virtual RESTFull API')
+    .setDescription(
+      'Documentación oficial de la RESTFull API de Biblioteca virtual',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
